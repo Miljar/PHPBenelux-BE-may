41781e7d26671bb011d80d9b9ed3f60e7ac8871c
Tom Van Herreweghe <tom@theanalogguy.be>
2011-04-28 15:05:22 +0200

    Tested existence of round(); tested amount of arguments for round()

--- tests/Utility.js
+++ tests/Utility.js
@@ -11,9 +11,25 @@ $(function(){
     /**
      * Verify existence of the utility object:
      */
-    test("instantiation", function() {
-        expect(2);
+    test("instantiation", function testInstantiation() {
+        expect(3);
+		
+		ok(typeof this.utility == "object", "Verifying if intantiation worked");
+		
         equals(typeof this.utility, "object", "Verifying existence of Utility object");
         equals(typeof this.utility.constructor, "function", "Verifying constructor type");
     });
+	
+	/**
+	 * Verify the working of the 
+	 */
+	test("round()", function testRound() {
+		expect(2);
+		
+		// Verify if method exists, and that it takes 2 parameters
+		equals(typeof this.utility.round, "function", "Verify existence of the method");
+		equals(2, this.utility.round.prototype.constructor.length, "Amount of arguments");
+		
+		
+	});
 });
